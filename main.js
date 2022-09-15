b79450da237719c1f91494f1de7b4867859b2c6e
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-09-15 01:14:37 +0000

    MVP completed

--- main.js
+++ main.js
@@ -4,26 +4,54 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-let images = ['../images/pic1.jpg', '../images/pic2.jpg', '../images/pic3.jpg', '../images/pic4.jpg', '../images/pic5.jpg'];
+
+const images = ['../images/pic1.jpg', '../images/pic2.jpg', '../images/pic3.jpg', '../images/pic4.jpg', '../images/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+
 const altTXT = {
-  '../images/pic1.jpg' : 'Dis be an eyeball',
-  '../images/pic2.jpg' : 'Melty rock thing',
-  '../images/pic3.jpg' : 'So according to Dungeon and Daddies, the purple ones make you levitate and the white ones make you forget the last 5 minutes',
-  '../images/pic4.jpg' : 'Stage of the 1999 cinematic masterpiece \"The Mummy\" starring Brendan fraser and Rachel Weisz.', 
-  '../images/pic5.jpg' : 'Hail Mothra'
-}
+  '../pic1.jpg' : 'Dis be an eyeball',
+  '../pic2.jpg' : 'Melty rock thing',
+  '../pic3.jpg' : 'So according to Dungeon and Daddies, the purple ones make you levitate and the white ones make you forget the last 5 minutes',
+  '../pic4.jpg' : 'Stage of the 1999 cinematic masterpiece \"The Mummy\" starring Brendan fraser and Rachel Weisz.', 
+  '../pic5.jpg' : 'Hail Mothra'
+};
+
 /* Looping through images */
-for (const image of images) {
+
+for (let i = 0; i < 5; i++) {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', images[image]);
-newImage.setAttribute('alt', altTXT[image]);
+newImage.setAttribute('src', images[i]);
+newImage.setAttribute('alt', altTXT[i]);
 thumbBar.appendChild(newImage);
 newImage.addEventListener('click', () => {
-    displayedImage.setAttribute('src', images[image]);
-    displayedImage.setAttribute('alt', images[image]);
+    displayedImage.setAttribute('src', images[i]);
+    displayedImage.setAttribute('alt', images[i]);
 });
 };
 
 //Wiring up the Darken/Lighten button
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+        if (btnClass === 'dark') {
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        } 
+        else  if (btnClass === 'light') {
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+        }
+});
+
+/* document.addEventListener('keyboardEvent', () => {
+    if ('keyUp') {
+        displayedImage.setAttribute('src', images[i++]);
+    } else if ('keyDown') {
+        displayedImage.setAttribute('src', images[i--]);
+    }
+  });
+*/
+  
